fd484fd2b6fcf423659d8c7f3800867cd7b3d8ad
Sebastian Poeplau <sebastian.poeplau@eurecom.fr>
2018-07-25 14:56:39 +0200

    Fix PTree::remove to clean the tree properly
    The previous version left unnecessary intermediate nodes behind, sometimes
    leading to very long paths in the tree.

--- lib/Core/PTree.cpp
+++ lib/Core/PTree.cpp
@@ -46,6 +46,29 @@ void PTree::remove(Node *n) {
     delete n;
     n = p;
   } while (n && !n->left.getPointer() && !n->right.getPointer());
+
+  if (n) {
+    // We're now at a node that has exactly one child; we've just deleted the
+    // other one. Eliminate the node and connect its child to the parent
+    // directly (if it's not the root).
+    auto child = n->left.getPointer() ? n->left : n->right;
+    Node *parent = n->parent;
+
+    child.getPointer()->parent = parent;
+    if (!parent) {
+      // We're at the root.
+      root = child;
+    } else {
+      if (n == parent->left.getPointer()) {
+        parent->left = child;
+      } else {
+        assert(n == parent->right.getPointer());
+        parent->right = child;
+      }
+    }
+
+    delete n;
+  }
 }
 
 void PTree::dump(llvm::raw_ostream &os) {
