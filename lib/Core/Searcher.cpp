1c8160fa5c2b1162ffeedda139639043b84160b8
Timotej Kapus <tk1713@ic.ac.uk>
2020-04-22 10:43:44 +0100

    RP deletion respects subsets and Pending deletion deletes pending states first

--- lib/Core/Searcher.cpp
+++ lib/Core/Searcher.cpp
@@ -327,9 +327,13 @@ std::vector<ExecutionState *> RandomPathSearcher::selectForDelition(int size) {
     std::vector<ExecutionState* > ret;
     ret.reserve(size);
     for(auto& es : executor.states) {
-        size--;
-        if(size < 1) break;
-        ret.push_back(es);
+      auto pnode = es->ptreeNode;
+      auto parentKid = pnode == pnode->parent->right.getPointer() ? pnode->parent->right : pnode->parent->left;
+      if(IS_OUR_NODE_VALID(parentKid)){ 
+          size--;
+          if(size < 1) break;
+          ret.push_back(es);
+      } 
     }
     return ret;
 }
@@ -497,7 +501,14 @@ std::vector<ExecutionState *> PendingSearcher::selectForDelition(int size) {
   //  if(basePendingSearcher->empty()) {
   //      klee_warning("Deleted as many pending states as possible");
   //  }
-    return baseNormalSearcher->selectForDelition(size);
+    auto ret = basePendingSearcher->selectForDelition(size);
+    size -= ret.size();
+    if(size > 0) {
+     auto retNormal =  baseNormalSearcher->selectForDelition(size);
+     ret.insert(ret.end(), retNormal.begin(), retNormal.end());
+
+    }
+    return ret;
 }
 
 bool PendingSearcher::empty() { 
